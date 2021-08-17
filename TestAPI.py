e5d66375a3bbe46be9b48a4e99f09bd9b85e0f17
Caden Popps <cadenpopps@gmail.com>
2021-08-17 14:11:38 -0500

    Functions working

--- TestAPI.py
+++ TestAPI.py
@@ -2,15 +2,36 @@
 import requests
 import json
 
-url = "https://public-api.tracker.gg/v2/splitgate/standard/profile/{}/{}"
-platform = "steam"
-platformUserIdentifier = "76561198320465710"
 
-headers = {
-        "TRN-API-Key": "f47e6a25-e2ba-47f0-b711-2de0d13d9629"
-        }
+def getAPIKey():
+    with open(".TRN-API-Key", "r") as f:
+        return f.read().strip()
 
-response = requests.get(url.format(platform, platformUserIdentifier), headers=headers)
+def getHeaders():
+    # headers = { "TRN-API-Key": getAPIKey() }
+    return { "TRN-API-Key": getAPIKey() }
 
-userProfile = json.loads(response.text)
-print(json.dumps(userProfile, indent=4))
+def searchUser(queryString):
+    url = "https://public-api.tracker.gg/v2/splitgate/standard/search"
+    params = {
+            "platform": "steam",
+            "query": queryString
+            }
+    headers=getHeaders()
+    response = requests.get(url, params=params, headers=headers)
+    searchResults = json.loads(response.text)
+    print(json.dumps(searchResults, indent=4))
+
+
+def getUserStats(playerIdentifier):
+    platform = "steam"
+    platformUserIdentifier = playerIdentifier
+    url = "https://public-api.tracker.gg/v2/splitgate/standard/profile/{}/{}".format(platform, platformUserIdentifier)
+    headers = getHeaders()
+    response = requests.get(url.format(platform, platformUserIdentifier), headers=headers)
+    userStats = json.loads(response.text)
+    print(json.dumps(userStats, indent=4))
+
+
+searchUser("Cpapi")
+getUserStats("76561198320465710")
